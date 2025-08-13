50cea478c749e734c080f332feb45b0f8b68def5
Ram Vishnu Bhumbar <rambhumbar15@gmail.com>
2025-08-13 13:46:04 +0530

    Add files via upload

---
+++ Classroom_program/Demo05.java
@@ -0,0 +1,13 @@
+package JavaPrograms;
+
+public class Demo05 {
+    
+	int i=1;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Demo05 A=new Demo05();
+    for(A.i;i<=10;i++) {
+    System.out.println("Hello \n");
+	}
+	}
+}
