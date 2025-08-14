de9740ad5629c0fdcc60f8343eb22785e49d812e
Ram Vishnu Bhumbar <rambhumbar15@gmail.com>
2025-08-14 11:05:18 +0530

    Update and rename Add.java to Q5.java

--- Classroom_program/Q5.java
+++ Classroom_program/Q5.java
@@ -1,7 +1,7 @@
 package Classroom_program;
 
 
-public class Add {
+public class Q5 {
 	public static void main(String[] args) {
 		int x=10;
 		int r=x+3;
@@ -9,4 +9,5 @@ public class Add {
 
 	}
 }
+
 
