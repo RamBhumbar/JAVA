f6b2273410a2091f70859679c10ae9b54bf26ea6
Ram Vishnu Bhumbar <rambhumbar15@gmail.com>
2025-08-14 11:22:45 +0530

    Delete Classroom_program directory

--- Classroom_program/Q1.java
+++
@@ -1,8 +0,0 @@
-package Classroom_program;
-
-public class Q1 {
-	 int a=20;
-	   int b=10;
-}
-
-
--- Classroom_program/Q7.java
+++
@@ -1,9 +0,0 @@
-package Classroom_program;
-
-public class Q7 extends BCA{
-	void count() {
-		System.out.println("Count of Sybca student is 40");
-	}
-}
-
-
